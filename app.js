a15e40f96bb346be1f01514ea7f733c92b64be17
TeresitaFraszczak <110676528+TeresitaFraszczak@users.noreply.github.com>
2023-08-23 03:06:22 -0300

    final

--- app.js
+++ app.js
@@ -14,6 +14,7 @@ import logger from 'morgan'
 import indexRouter from './routes/index.js'
 import notFoundHandler from './middlewares/notFoundHandler.js'
 import errorHandler from './middlewares/errorHandler.js'
+import cors from 'cors'
 
 let app = express();
 //var usersRouter = require('./routes/users');
@@ -28,7 +29,7 @@ app.set('view engine', 'ejs');
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-//app.use(cookieParser());
+app.use(cors());
 app.use(express.static(path.join(__dirname, 'public')));
 
 //ROUTER
--- controllers/cities/read.js
+++ controllers/cities/read.js
@@ -2,7 +2,21 @@ import City from "../../models/City.js";
 
 export default async (req,res)=>{
     try {
-        let allCities = await City.find()
+        let busqueda = {};
+        let ordenamiento = {};
+
+        if(req.query.admin_){
+            busqueda.admin_id = req.query.admin_id;
+        }
+        if(req.query.city){
+           busqueda.city = new RegExp('^'+req.query.city,'i');
+        }
+        if(req.query.sort){
+            ordenamiento.city = req.query.sort;
+        }
+        let allCities = await City.find(busqueda, 'country city photo smalldescription admin_id')
+        .populate('admin_id','photo name mail -_id')
+        .sort(ordenamiento)
         return res.status(200).json({
             success: true,
             message: 'cities found',
